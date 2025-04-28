b017451f4129b1ccbef30ab6db4540e5f5182d37
Prestonbusch <pbusch@uark.edu>
2025-04-28 17:57:54 -0500

    Update main_app4.py

--- main_app4.py
+++ main_app4.py
@@ -353,13 +353,13 @@ def load_data(ticker, start, end):
                     return None
                 
                 # Display a sample of the data
-              if st.checkbox("Show sample of loaded data", value=False):
-                  st.subheader("Data Summary")
-                  st.write("**Columns:**", data.columns.tolist())
-                  st.write("**First 3 Rows:**")
-                  st.dataframe(data.head(3))
-                  st.write("**Last 3 Rows:**")
-                  st.dataframe(data.tail(3))
+                    if st.checkbox("Show sample of loaded data", value=False):
+                      st.subheader("Data Summary")
+                      st.write("**Columns:**", data.columns.tolist())
+                      st.write("**First 3 Rows:**")
+                      st.dataframe(data.head(3))
+                      st.write("**Last 3 Rows:**")
+                      st.dataframe(data.tail(3))
 
                 
                 return data
